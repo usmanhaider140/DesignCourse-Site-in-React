0c87fbc0eb06d2b4244cf80fdda437be0f19ea28
usmanhaider.falconit <usmanhaider.falconit@gmail.com>
2021-06-17 11:07:30 +0500

    git init

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,17 +8,12 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <h1>Welcome to Designing in React</h1>
+    <p>
+      Here we will learn react in gatsby and Design a beautiful, awesome,
+      astonished website.😘
+    </p>
+
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
