6b70f4a83109325b51d1565bd197e3aa1f546973
usmanhaider.falconit <usmanhaider.falconit@gmail.com>
2021-06-18 10:20:32 +0500

    new layout

---
+++ src/components/layout/layout.js
@@ -0,0 +1,29 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import "./layout.css"
+import { Link } from "gatsby"
+import Header from "./Header/Header"
+
+const Layout = () => {
+  return (
+    <>
+      <div className="Hero">
+        <Header />
+        <div className="HeroGroup">
+          <h1>Learning to Design and Code in React</h1>
+          <p>
+            Complete courses about the best tools and design systems. Prototypes
+            and build apps with React and Node Js
+          </p>
+          <Link to="/page-2/">Watch the Video</Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
